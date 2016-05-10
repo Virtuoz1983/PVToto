063a27b5a6047bf9850e04875d557f1829a9b71b
PavloY <pavlo.yakovlev@gmail.com>
2016-05-10 18:39:46 +0300

    #Temporary tables
    conversion of temporary table

--- BD/Table/dbo.Tot_RecreateAllVarTables.sql
+++ BD/Table/dbo.Tot_RecreateAllVarTables.sql
@@ -3,6 +3,7 @@
 
 		Yakovlev P.V.   21.04.2016  - created this procedure
 
+		Yakovlev P.V.   10.05.2016 - added 
 */
 
 USE [TotDB]
@@ -14,88 +15,125 @@ CREATE PROCEDURE [dbo].[Tot_RecreateAllVarTables]
 AS 
 BEGIN
 
-	DROP TABLE [dbo].[Tot_var1]
-	CREATE TABLE [dbo].[Tot_var1]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY]
+	IF OBJECT_ID ('tempdb..#Tot_var1', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var1
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var1 
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
 
-
-	DROP TABLE [dbo].[Tot_var2]
-
-	CREATE TABLE [dbo].[Tot_var2]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY]
-
-	DROP TABLE [dbo].[Tot_var3]
-	CREATE TABLE [dbo].[Tot_var3]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-	DROP TABLE [dbo].[Tot_var4]
-
-	CREATE TABLE [dbo].[Tot_var4]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-
-	DROP TABLE [dbo].[Tot_var5]
-	CREATE TABLE [dbo].[Tot_var5]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-
-
-	DROP TABLE [dbo].[Tot_var6];
-	CREATE TABLE [dbo].[Tot_var6]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-
-	DROP TABLE [dbo].[Tot_var7];
-	CREATE TABLE [dbo].[Tot_var7]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-
-	DROP TABLE [dbo].[Tot_var8];
-	CREATE TABLE [dbo].[Tot_var8]
-		(
-		  [id] [INT] IDENTITY(1, 1)
-					 NOT NULL ,
-		  [n] [NVARCHAR](10) NULL ,
-		  [tirag] [INT] NULL
-		)
-	ON  [PRIMARY];
-
-END
+	IF OBJECT_ID ('tempdb..#Tot_var2', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var2
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var2 
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var3', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var3
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var3
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var4', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var4
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var4
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var5', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var5
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var5
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var6', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var6
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var6
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var7', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var7
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var7
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	IF OBJECT_ID ('tempdb..#Tot_var8', 'U')
+		IS NOT NULL
+		TRUNCATE TABLE #Tot_var8
+	ELSE
+		BEGIN  
+			CREATE TABLE #Tot_var8
+				(
+				  [id] [INT] IDENTITY(1, 1)
+							 NOT NULL ,
+				  [n] [NVARCHAR](10) NULL ,
+				  [tirag] [INT] NULL
+				)
+				ON  [PRIMARY]
+		END
+	
+	END
 GO
