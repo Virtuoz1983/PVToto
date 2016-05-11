e15c0a922f9009b80d10ad35f942f6016eac384d
PavloY <pavlo.yakovlev@gmail.com>
2016-05-11 09:29:36 +0300

    Added work with temporary tables
    Added work with temporary tables in dbo.Tot_InsertKuponsbyTirag

--- BD/Table/dbo.Tot_InsertKuponsbyTirag.sql
+++ BD/Table/dbo.Tot_InsertKuponsbyTirag.sql
@@ -15,13 +15,13 @@ CREATE PROCEDURE [dbo].[Tot_InsertKuponsbyTirag](@KoefForTirag INT = 1)
 		BEGIN
 			INSERT INTO [dbo].[Tot_Kupons]([tirag],[var1],[var2],[var3],[var4],[var5],[var6],[var7],[var8])
 			SELECT tv1.tirag*@KoefForTirag, tv1.n, tv2.n, tv3.n, tv4.n, tv5.n, tv6.n, tv7.n, tv8.n
-			FROM Tot_var1 tv1
-			 JOIN Tot_var2 tv2 ON tv1.id = tv2.id
-			 JOIN Tot_var3 tv3 ON tv1.id = tv3.id
-			 JOIN Tot_var4 tv4 ON tv1.id = tv4.id
-			 JOIN Tot_var5 tv5 ON tv1.id = tv5.id
-			 JOIN Tot_var6 tv6 ON tv1.id = tv6.id
-			 JOIN Tot_var7 tv7 ON tv1.id = tv7.id
-			 JOIN Tot_var8 tv8 ON tv1.id = tv8.id
+			FROM #Tot_var1 tv1
+			 JOIN #Tot_var2 tv2 ON tv1.id = tv2.id
+			 JOIN #Tot_var3 tv3 ON tv1.id = tv3.id
+			 JOIN #Tot_var4 tv4 ON tv1.id = tv4.id
+			 JOIN #Tot_var5 tv5 ON tv1.id = tv5.id
+			 JOIN #Tot_var6 tv6 ON tv1.id = tv6.id
+			 JOIN #Tot_var7 tv7 ON tv1.id = tv7.id
+			 JOIN #Tot_var8 tv8 ON tv1.id = tv8.id
 		END
 GO
