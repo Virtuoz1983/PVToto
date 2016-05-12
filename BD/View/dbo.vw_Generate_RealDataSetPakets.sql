35321bff2bd16288b2d1122fc0fb26b68f9d83a4
PavloY <pavlo.yakovlev@gmail.com>
2016-05-12 17:27:31 +0300

    modified to view
    modified to view :
    dbo.vw_Generate_RealDataSetPakets
    dbo.vw_Generate_RealDataSets
    dbo.vw_TabloMatches

--- BD/View/dbo.vw_Generate_RealDataSetPakets.sql
+++ BD/View/dbo.vw_Generate_RealDataSetPakets.sql
@@ -2,27 +2,51 @@
 		Internal system tables with vw_Generate_RealDataSetPakets
 
 		Yakovlev P.V.   20.04.2016  - created this table
-
+						12.05.2016  - modified to view
 */
 
-USE [TotDB]
-IF OBJECT_ID('dbo.vw_Generate_RealDataSetPakets', 'U') IS NULL
-    BEGIN
-		CREATE TABLE [dbo].[vw_Generate_RealDataSetPakets]
-			(
-			[id] [int] IDENTITY(1,1) NOT NULL, -- внесли
-			[tirag] [int] NULL,
-			[paket] [int] NULL,
-			[NumMatch] [int] NULL,
-			[Koef] [numeric](9, 2) NULL,
-			[Kol] [int] NULL,
-			[Result] [varchar](3) NOT NULL,
-			[orderby] [bigint] NULL,
-			[paket_kol] [int] NULL,
-					CONSTRAINT [PK_vw_Generate_RealDataSetPakets_ID] PRIMARY KEY CLUSTERED
-					([id] ASC)
-					WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF,
-					IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-			) ON [PRIMARY]
-    END;
-GO
+USE TotDB
+IF OBJECT_ID('dbo.vw_Generate_RealDataSetPakets', 'V') IS NOT NULL EXECUTE('DROP VIEW dbo.vw_Generate_RealDataSetPakets')
+GO    
+
+CREATE VIEW [dbo].[vw_Generate_RealDataSetPakets] 
+	AS
+		SELECT tm.tirag, paket, tm.NumMatch, tm.Koef, rd.Kol, tm.Result, 
+		ROW_NUMBER() OVER (PARTITION BY tm.tirag, paket, tm.NumMatch ORDER BY tm.Koef) AS orderby,
+		SUM(Kol) OVER (PARTITION BY tm.tirag, paket, tm.NumMatch) AS paket_kol    
+		FROM vw_TabloMatches tm
+			LEFT JOIN 
+				(
+					SELECT  tirag, paket, 1 AS NumMatch, var1 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var1 
+						UNION ALL 
+					SELECT  tirag, paket, 2 AS NumMatch, var2 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var2
+						UNION ALL 
+					SELECT  tirag, paket, 3 AS NumMatch, var3 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var3
+						UNION ALL 
+					SELECT  tirag, paket, 4 AS NumMatch, var4 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var4
+						UNION ALL 
+					SELECT  tirag, paket, 5 AS NumMatch, var5 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var5
+						UNION ALL 
+					SELECT  tirag, paket, 6 AS NumMatch, var6 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var6
+						UNION ALL 
+					SELECT  tirag, paket, 7 AS NumMatch, var7 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var7
+						UNION ALL 
+					SELECT  tirag, paket, 8 AS NumMatch, var8 AS Result, COUNT(1) AS Kol
+					FROM [dbo].[Tot_RealData]  
+					GROUP BY tirag, paket, var8
+				) rd ON tm.tirag = rd.tirag AND tm.NumMatch = rd.NumMatch AND tm.Result = rd.Result 
+				WHERE /*tm.tirag = 1868 AND tm.NumMatch = 1 AND paket = 100 AND*/ rd.Kol IS NOT NULL
--- BD/View/dbo.vw_TabloMatches.sql
+++ BD/View/dbo.vw_TabloMatches.sql
@@ -2,24 +2,64 @@
 		Internal system tables with vw_TabloMatches
 
 		Yakovlev P.V.   20.04.2016  - created this table
-
+						12.05.2016  - modified to view
 */
 
-USE [TotDB]
-IF OBJECT_ID('dbo.vw_TabloMatches', 'U') IS NULL 
-    BEGIN
-		CREATE TABLE [dbo].[vw_TabloMatches] 
-			(
-			[id] [int] IDENTITY(1,1) NOT NULL, 
-			[tirag] [int] NULL,
-			[NumMatch] [int] NULL,
-			[Result] [varchar](3) NOT NULL,
-			[Koef] [numeric](9, 2) NULL,
-					CONSTRAINT [PK_vw_TabloMatches_ID] PRIMARY KEY CLUSTERED 
-					([id] ASC)
-					WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-					IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-			)
-			ON [PRIMARY];
-    END;
-GO
+USE TotDB
+IF OBJECT_ID('dbo.vw_TabloMatches', 'V') IS NOT NULL EXECUTE('DROP VIEW dbo.vw_TabloMatches')
+GO 
+CREATE VIEW [dbo].[vw_TabloMatches]
+	AS
+		SELECT tirag, m.NumMatch, '0:1' AS Result, v01  AS Koef
+		FROM [dbo].[Tot_Matches] m
+		--WHERE tirag = 1891 AND m.NumMatch = 1
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '0:2' AS Result, v02  AS Koef
+		FROM [dbo].[Tot_Matches] m
+		--WHERE tirag = 1891 AND m.NumMatch = 1
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '1:2' AS Result, v12  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '0:3' AS Result, v03  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '1:3' AS Result, v13  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '2:3' AS Result, v23  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '0:0' AS Result, v00  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '1:1' AS Result, v11  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '2:2' AS Result, v22  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '3:3' AS Result, v33  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '1:0' AS Result, v10  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '2:0' AS Result, v20  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '2:1' AS Result, v21  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '3:0' AS Result, v30  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '3:1' AS Result, v31  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, '3:2' AS Result, v32  AS Koef
+		FROM [dbo].[Tot_Matches] m
+			UNION ALL 
+		SELECT tirag, m.NumMatch, 'LD' AS Result, vLD  AS Koef
+		FROM [dbo].[Tot_Matches] m
+		GO
