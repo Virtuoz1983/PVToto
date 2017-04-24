66894508c8000d2053997c8fa4a2462a2dec9c12
SOFTSERVE\vteresch <vteresch@softserveinc.com>
2017-04-24 17:59:14 +0300

    Added new BI project. Added new dimenssion, new stored procedure for storing championates.

--- BD/SP/dbo.HistoryTotalsMT_Merge.sql
+++ BD/SP/dbo.HistoryTotalsMT_Merge.sql
@@ -1,6 +1,9 @@
 /*
         Tereshenko V.A. 08.06.2016
 		Merge data from Stage
+
+		Tereshenko V.A. 20.04.2017 - added update statemant for "sect" field
+		Tereshenko V.A. 24.04.2017 - added delete statemant (as for now just for testing purpose). Will remove after.
 		
  USAGE:   
     exec dbo.HistoryTotalsMT_Merge
@@ -19,7 +22,7 @@ GO
 
 ALTER PROCEDURE dbo.HistoryTotalsMT_Merge
 AS
-    BEGIN 
+        BEGIN 
         SET NOCOUNT ON
         
         MERGE INTO dbo.HistoryTotalsMT AS Tdest
@@ -237,4 +240,63 @@ AS
                      Tsrc.poolld
                    );
 
-    END 
+
+update h
+  set sect  = --case when 
+		  case		  
+			 when CHARINDEX(N'Кубок Флориды',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Кубок Флориды',sect)-2)
+			 when CHARINDEX(N'Кирин Кап',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Кирин Кап',sect)-2)
+			 when CHARINDEX(N'2017. Финальный',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'2017. Финальный',sect)-2)
+			 when CHARINDEX(N'2016. Финальный',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'2016. Финальный',sect)-2)
+			 when CHARINDEX(N'2016. Женщины',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'2016. Женщины',sect)-1)			 
+			 when CHARINDEX(N'2016. Мужчины',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'2016. Мужчины',sect)-1)			 
+			 when CHARINDEX(N'ЧЕ-2013.',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'ЧЕ-2013.',sect)-1)			 
+			 when CHARINDEX(N'Групповой этап',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Групповой этап',sect)-2)
+			 when CHARINDEX(N'Матчи за право',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Матчи за право',sect)-2)			 
+			 when CHARINDEX(N'1/32 финала',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1/32 финала',sect)-2)
+			 when CHARINDEX(N'1/16 финала',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1/16 финала',sect)-2)
+			 when CHARINDEX(N'1/8 финала',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1/8 финала',sect)-2)
+			 when CHARINDEX(N'1/2 финала',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1/2 финала',sect)-2)
+			 when CHARINDEX(N'1/4 финала',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1/4 финала',sect)-2)
+			 when CHARINDEX(N'7-й раунд',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'7-й раунд',sect)-2)
+			 when CHARINDEX(N'4-й раунд',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'4-й раунд',sect)-2)
+			 when CHARINDEX(N'3-й раунд',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'3-й раунд',sect)-2)
+			 when CHARINDEX(N'2-й раунд',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'2-й раунд',sect)-2)
+			 when CHARINDEX(N'1-й раунд',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'1-й раунд',sect)-2)
+			 when CHARINDEX(N'Плей-офф за',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Плей-офф за',sect)-2)
+			 when CHARINDEX(N'Плей-офф',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Плей-офф',sect)-2)
+			 when CHARINDEX(N'Матчи за звание',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Матчи за звание',sect)-2)
+			 when CHARINDEX(N'Классификационный раунд.',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Классификационный раунд.',sect)-2)
+			 when CHARINDEX(N'Раунд плей-офф',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Раунд плей-офф',sect)-2)
+			 when CHARINDEX(N'Кубок лиги.',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Кубок лиги.',sect)-2)
+			 when CHARINDEX(N'Финал.',sect) > 0 then SUBSTRING(sect,0,CHARINDEX(N'Финал.',sect)-2)
+			 else sect
+		  end,
+		  winprob = case when [exodus] = '1:0' then [prob10] 
+			 	    when [exodus] = '2:0' then [prob20] 
+				    when [exodus] = '2:1' then [prob21] 
+					when [exodus] = '3:0' then [prob30] 
+					when [exodus] = '3:1' then [prob31] 
+					when [exodus] = '3:2' then [prob32] 
+					when [exodus] = '0:0' then [prob00] 
+					when [exodus] = '1:1' then [prob11] 
+					when [exodus] = '2:2' then [prob22] 
+					when [exodus] = '3:3' then [prob33] 
+					when [exodus] = '0:1' then [prob01] 
+					when [exodus] = '0:2' then [prob02] 
+					when [exodus] = '1:2' then [prob12] 
+					when [exodus] = '0:3' then [prob03] 
+					when [exodus] = '1:3' then [prob13] 
+					when [exodus] = '2:3' then [prob23] 
+					when [exodus] = 'LD'  then [probLD] 
+					when [exodus] = 'Отмена' then 1000
+						else -1.0 
+			end
+	from [dbo].[HistoryTotalsMT] h
+
+	-- As for now I removed data from 2227 tirag
+	-- Because we have empty values for match Санкт-Петербург до 17 - Иран до 17<
+	delete from [dbo].[HistoryTotalsMT] where tirag = 2227
+
+
+END 
