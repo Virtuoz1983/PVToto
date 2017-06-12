46d4877b4d04985b206e983a5d6adf1d7dba2d68
SOFTSERVE\vteresch <vteresch@softserveinc.com>
2017-06-12 10:42:44 +0300

    Changed function GetCalculatedKoefPerTirag Created new visualization for test results Changed SSIS project (minor chanes)

--- BD/FN/GetCalculatedKoefPerTirag.sql
+++ BD/FN/GetCalculatedKoefPerTirag.sql
@@ -3,6 +3,7 @@
 This function allow to calculate coef per tirag according to provided countes per kategory
 
 	Tereshenko V.A.  02.08.2016   - created this function
+	Tereshenko V.A.  09.06.2017   - added 2 new cases. For tirages with 1 canceled macth and for tirages with 2 canceled matches.
 
 EXAMPLE: 
 select [dbo].[GetCalculatedKoefPerTirag] (2060 , 1000 , 0, 0, 0, 1, 10, 50, 200)
@@ -28,11 +29,20 @@ BEGIN
 DECLARE @CancelCount INT = 0;
 DECLARE @NewPool NUMERIC(15,5), @Pool8 NUMERIC(15,5), @Pool7 NUMERIC(15,5), @Pool6 NUMERIC(15,5), @Pool5 NUMERIC(15,5), @Pool4 NUMERIC(15,5), @Pool3 NUMERIC(15,5), @Pool2 NUMERIC(15,5);
 DECLARE @BetAmount NUMERIC(15,5) = 0.2;
+DECLARE @JackPot NUMERIC(15,5)
 
 -- Check is cancelation exist
 SELECT @CancelCount = COUNT(1)
 FROM [dbo].[HistoryTotalsMT]
 WHERE tirag = @Tirag AND exodus = N'Отмена'
+
+select @NewPool = [pool] + @KuponsCount*@BetAmount
+	  FROM [dbo].[HistoryTotalsMT]
+WHERE tirag = @Tirag AND numb = 1
+
+SELECT @JackPot = jackPot
+      FROM [dbo].[HistoryTotalsMT]
+WHERE tirag = @Tirag AND numb = 1
 
 IF @CancelCount > 2
 BEGIN 
@@ -40,23 +50,13 @@ BEGIN
 	SELECT @Tirag, -1, -1, -1, -1, -1, -1, -1 
 END 
 
-select @NewPool = [pool] + @KuponsCount*@BetAmount
-FROM [dbo].[HistoryTotalsMT]
-WHERE tirag = @Tirag AND numb = 1
-
--- // Minus profit for BK
---SET @NewPool = @NewPool*0.95
-SELECT @Pool8 = @NewPool*0.05, @Pool7 = @NewPool*0.05, @Pool6 = @NewPool*0.075, @Pool5 = @NewPool*0.10, @Pool4 = @NewPool*0.125,
-	   @Pool3 = @NewPool*0.20, @Pool2 = @NewPool*0.35;	   
-
 IF @CancelCount < 1 
 BEGIN
 
-DECLARE @JackPot NUMERIC(15,5)
-
-	SELECT @JackPot = jackPot
-	FROM [dbo].[HistoryTotalsMT]
-	WHERE tirag = @Tirag AND numb = 1
+	-- // Minus profit for BK
+	--SET @NewPool = @NewPool*0.95
+	SELECT @Pool8 = @NewPool*0.05, @Pool7 = @NewPool*0.05, @Pool6 = @NewPool*0.075, @Pool5 = @NewPool*0.10, @Pool4 = @NewPool*0.125,
+		   @Pool3 = @NewPool*0.20, @Pool2 = @NewPool*0.35;	   
 
 	INSERT INTO @retFindReports
 	SELECT @Tirag,
@@ -108,9 +108,121 @@ DECLARE @JackPot NUMERIC(15,5)
 	
 END  -- IF @CancelCount < 1 
 
+IF @CancelCount = 1 
+BEGIN
+    -- 1 несостоявшийся матч – распределяется 25% Джек-пота (только за 7 угаданных матчей);
+
+	SELECT @Pool7 = @NewPool*0.05, @Pool6 = @NewPool*0.05, @Pool5 = @NewPool*0.075, @Pool4 = @NewPool*0.1,
+		   @Pool3 = @NewPool*0.125, @Pool2 = @NewPool*0.55;	   
+  
+   	INSERT INTO @retFindReports
+	SELECT @Tirag,
+	       -- we canot have 8 for this case
+		   -- CASE WHEN quan8 + @CountBet8 > 0 THEN (@Pool8 + @JackPot*0.95)/(quan8 + @CountBet8)/@BetAmount ELSE 0 END AS Koef8,
+		   0 AS Koef8,
+		   CASE -- we have 7, without 8 
+				WHEN (quan7 + @CountBet7 > 0) THEN (@Pool7 + @JackPot*0.25)/(quan7 + @CountBet7)/@BetAmount	
+		   ELSE 0 END AS Koef7,
+		   CASE -- we have 6, without 7 
+				WHEN (quan6 + @CountBet6 > 0) AND (quan7 + @CountBet7 = 0) THEN (@Pool6 + @Pool7*0.3)/(quan6 + @CountBet6)/@BetAmount
+				-- we have 6, with 7
+				WHEN (quan6 + @CountBet6 > 0) AND (quan7 + @CountBet7 > 0) THEN (@Pool6)/(quan6 + @CountBet6)/@BetAmount				
+		   ELSE 0 END AS Koef6,
+		   CASE -- we have 5, without 6,7
+				WHEN (quan5 + @CountBet5 > 0) AND (quan6 + @CountBet6 = 0) AND (quan7 + @CountBet7 = 0) THEN (@Pool5 + (@Pool6 + @Pool7)*0.3)/(quan5 + @CountBet5)/@BetAmount				
+				-- we have 5, with 6
+				WHEN (quan5 + @CountBet5 > 0) AND (quan6 + @CountBet6 > 0) THEN (@Pool5)/(quan5 + @CountBet5)/@BetAmount				
+		   ELSE 0 END AS Koef5,
+		   CASE -- we have 4, without 5,6,7
+				WHEN (quan4 + @CountBet4 > 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0)  
+				AND (quan7 + @CountBet7 = 0) THEN (@Pool4 + @Pool5*0.3 + @Pool6*0.3 + @Pool7*0.3)/(quan4 + @CountBet4)/@BetAmount				
+				-- we have 4, with 5
+				WHEN (quan4 + @CountBet4 > 0) AND ((quan5 + @CountBet5 > 0) OR (quan6 + @CountBet6 > 0)) THEN (@Pool4)/(quan4 + @CountBet4)/@BetAmount				
+		   ELSE 0 END AS Koef4,
+		   CASE -- we have 3, without 4,5,6,7 
+				WHEN (quan3 + @CountBet3 > 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				AND (quan7 + @CountBet7 = 0) THEN (@Pool3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3 + @Pool7*0.3)/(quan3 + @CountBet3)/@BetAmount				
+				-- we have 3, with 4
+				WHEN (quan3 + @CountBet3 > 0) AND ((quan4 + @CountBet4 > 0) OR (quan5 + @CountBet5 > 0) OR (quan6 + @CountBet6 > 0)) THEN (@Pool3)/(quan3 + @CountBet3)/@BetAmount				
+		   ELSE 0 END AS Koef3,
+		   CASE WHEN 
+		   CASE -- we have 2, without 3,4,5,6,7 
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 = 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				AND (quan7 + @CountBet7 = 0) THEN (@Pool2 + @Pool3*0.3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3 + @Pool7*0.3)/(quan2 + @CountBet2)/@BetAmount				
+				-- we have 2, with 3
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 > 0) THEN (@Pool2)/(quan2 + @CountBet2)/@BetAmount				
+		   ELSE 0 END < 1.1 THEN 1.1
+		   ELSE 
+		   CASE -- we have 2, without 3,4,5,6,7
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 = 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				AND (quan7 + @CountBet7 = 0) THEN (@Pool2 + @Pool3*0.3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3 + @Pool7*0.3)/(quan2 + @CountBet2)/@BetAmount				
+				-- we have 2, with 3
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 > 0) THEN (@Pool2)/(quan2 + @CountBet2)/@BetAmount				
+		   ELSE 0 END
+		   END AS Koef2
+	FROM [dbo].[HistoryTotalsMT]
+	WHERE tirag = @Tirag AND numb = 1;
+   
+END -- IF @CancelCount = 1 
+
+IF @CancelCount = 2
+BEGIN
+    -- 2 несостоявшихся матча – Джек-пот не разыгрывается.
+
+	SELECT @Pool6 = @NewPool*0.05, @Pool5 = @NewPool*0.05, @Pool4 = @NewPool*0.075,
+		   @Pool3 = @NewPool*0.1, @Pool2 = @NewPool*0.675;	   
+  
+   	INSERT INTO @retFindReports
+	SELECT @Tirag,
+	       -- we canot have 8 for this case		   
+		   0 AS Koef8,
+		   -- we canot have 7 for this case		   
+		   0 AS Koef7,		   
+		   CASE -- we have 6, without 7 
+				WHEN (quan6 + @CountBet6 > 0) THEN (@Pool6)/(quan6 + @CountBet6)/@BetAmount				
+		   ELSE 0 END AS Koef6,
+		   CASE -- we have 5, without 6
+				WHEN (quan5 + @CountBet5 > 0) AND (quan6 + @CountBet6 = 0) THEN (@Pool5 + @Pool6*0.3)/(quan5 + @CountBet5)/@BetAmount				
+				-- we have 5, with 6
+				WHEN (quan5 + @CountBet5 > 0) AND (quan6 + @CountBet6 > 0) THEN (@Pool5)/(quan5 + @CountBet5)/@BetAmount				
+		   ELSE 0 END AS Koef5,
+		   CASE -- we have 4, without 5,6
+				WHEN (quan4 + @CountBet4 > 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0)  
+				THEN (@Pool4 + @Pool5*0.3 + @Pool6*0.3)/(quan4 + @CountBet4)/@BetAmount				
+				-- we have 4, with 5
+				WHEN (quan4 + @CountBet4 > 0) AND ((quan5 + @CountBet5 > 0) OR (quan6 + @CountBet6 > 0)) THEN (@Pool4)/(quan4 + @CountBet4)/@BetAmount				
+		   ELSE 0 END AS Koef4,
+		   CASE -- we have 3, without 4,5,6 
+				WHEN (quan3 + @CountBet3 > 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				THEN (@Pool3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3)/(quan3 + @CountBet3)/@BetAmount				
+				-- we have 3, with 4
+				WHEN (quan3 + @CountBet3 > 0) AND ((quan4 + @CountBet4 > 0) OR (quan5 + @CountBet5 > 0) OR (quan6 + @CountBet6 > 0)) THEN (@Pool3)/(quan3 + @CountBet3)/@BetAmount				
+		   ELSE 0 END AS Koef3,
+		   CASE WHEN 
+		   CASE -- we have 2, without 3,4,5,6 
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 = 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				THEN (@Pool2 + @Pool3*0.3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3)/(quan2 + @CountBet2)/@BetAmount				
+				-- we have 2, with 3
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 > 0) THEN (@Pool2)/(quan2 + @CountBet2)/@BetAmount				
+		   ELSE 0 END < 1.1 THEN 1.1
+		   ELSE 
+		   CASE -- we have 2, without 3,4,5,6
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 = 0) AND (quan4 + @CountBet4 = 0) AND (quan5 + @CountBet5 = 0) AND (quan6 + @CountBet6 = 0) 
+				THEN (@Pool2 + @Pool3*0.3 + @Pool4*0.3 + @Pool5*0.3 + @Pool6*0.3)/(quan2 + @CountBet2)/@BetAmount				
+				-- we have 2, with 3
+				WHEN (quan2 + @CountBet2 > 0) AND (quan3 + @CountBet3 > 0) THEN (@Pool2)/(quan2 + @CountBet2)/@BetAmount				
+		   ELSE 0 END
+		   END AS Koef2
+	FROM [dbo].[HistoryTotalsMT]
+	WHERE tirag = @Tirag AND numb = 1;
+   
+END -- IF @CancelCount = 2
+
+
 RETURN
 
 END
+
 
 
 GO
